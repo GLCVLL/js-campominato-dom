fb6cb469d24435272287068d574994fb238d365d
GLCVLL <g.ioele96@outlook.it>
2023-06-08 16:32:10 +0200

    added user score to the function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
 console.log('JS OK');
 
-// Function to generate the game grid
-const generateGrid = () => {
-
-  playButton.innerText = 'Rigioca';
-
-  gridContainer.innerHTML = '';
-
-  for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement('div');
-    cell.className = 'cell';
-    cell.innerText = i;
-
-  // Event listener for cell click
-  cell.addEventListener('click', () => {
-      cell.classList.toggle('active');
-      console.log(`Clicked cell ${i}`);
-  });
-    
-  gridContainer.appendChild(cell);
-
-  }
-};
-
 // all the elements from the DOM
 const gridContainer = document.getElementById('my-grid');
 
 const playButton = document.getElementById('play-button');
 
+
+// Aggiungo una variabile per tenere traccia del punteggio dell'utente
+let score = 0;
+
+// Funzione per generare la griglia di gioco
+const generateGrid = () => {
+  playButton.innerText = 'Rigioca';
+  gridContainer.innerHTML = '';
+
+for (let i = 1; i <= 100; i++) {
+  const cell = document.createElement('div');
+  cell.className = 'cell';
+  cell.innerText = i;
+
+// Event listener per il click sulla cella
+cell.addEventListener('click', () => {
+  // Controllo se la cella è già attiva
+  if (!cell.classList.contains('active')) {
+    // Incremento il punteggio e aggiorna l'interfaccia utente
+    score++;
+    updateScore();
+
+    // Attivo/disattivo la classe 'active' sulla cella
+    cell.classList.toggle('active');
+    console.log(`Clicked cell ${i}`);
+
+    // Disabilita ulteriori click sulla cella
+    cell.style.pointerEvents = 'none';
+  }
+});
+
+    gridContainer.appendChild(cell);
+ }
+};
+
+// Funzione per aggiornare il punteggio nell'interfaccia utente
+const updateScore = () => {
+  const scoreText = document.getElementById('score-text');
+  scoreText.innerText = `Punteggio: ${score}`;
+};
 
 // listen to the play button
 playButton.addEventListener('click', generateGrid);
